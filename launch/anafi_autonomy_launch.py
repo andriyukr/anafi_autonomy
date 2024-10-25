85a31ace6c5adb2c4315781ef06a854b4038f871
andriyukr <andriyukr@gmail.com>
2024-10-25 14:38:19 +0200

    CMakeLists fix

--- launch/anafi_autonomy_launch.py
+++ launch/anafi_autonomy_launch.py
@@ -9,7 +9,6 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
